95756cf74eb7614dad863e0d25b69f3a4550bb97
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2025-02-11 14:43:41 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifsp/carlao2005/modelo/Produto.java
@@ -0,0 +1,50 @@
+package br.edu.ifsp.carlao2005.modelo;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    // Atributo que representa a data de cadastro:
+    private LocalDate dataCadastro = LocalDate.now();
+    // Categoria não é mais um @Enumerated, retiramos a anotação.
+    // Porém precisamos indicar a cardinalidade do relacionamento:
+    @ManyToOne
+    private Categoria categoria;
+
+    // Construtor default:
+    public Produto() {}
+
+    // Construtor (não precisa id nem dataCadastro):
+    public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public BigDecimal getPreco() { return preco; }
+
+    public void setPreco(BigDecimal preco) { this.preco = preco; }
+}
